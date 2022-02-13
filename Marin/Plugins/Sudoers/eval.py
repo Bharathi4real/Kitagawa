24883031e7940774948522af149afe57965bbbaf
• BHARATHI • <86936565+Bharathi2003@users.noreply.github.com>
2022-02-13 12:37:22 +0530

    Added Shell

--- Marin/Plugins/Sudoers/eval.py
+++ Marin/Plugins/Sudoers/eval.py
@@ -114,6 +114,42 @@ def do(func, bot, update):
         if result:
             return result
 
+@dev_plus
+def shell(update: Update, context: CallbackContext):
+    message = update.effective_message
+    cmd = message.text.split(" ", 1)
+    if len(cmd) == 1:
+        message.reply_text("No command to execute was given.")
+        return
+    cmd = cmd[1]
+    process = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    stdout, stderr = process.communicate()
+    reply = ""
+    stderr = stderr.decode()
+    if stdout := stdout.decode():
+        reply += f"*Stdout*\n`{stdout}`\n"
+        LOGGER.info(f"Shell - {cmd} - {stdout}")
+    if stderr:
+        reply += f"*Stderr*\n`{stderr}`\n"
+        LOGGER.error(f"Shell - {cmd} - {stderr}")
+    if len(reply) > 3000:
+        with open("shell_output.txt", "w") as file:
+            file.write(reply)
+        with open("shell_output.txt", "rb") as doc:
+            context.bot.send_document(
+                document=doc,
+                filename=doc.name,
+                reply_to_message_id=message.message_id,
+                chat_id=message.chat_id,
+            )
+    else:
+        message.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
+
 
 @dev_plus
 def clear(update: Update, context: CallbackContext):
@@ -135,10 +171,20 @@ EXEC_HANDLER = CommandHandler(
     execute,
     run_async=True,
 )
+SHELL_HANDLER = CommandHandler(["sh"], shell, run_async=True)
+
 CLEAR_HANDLER = CommandHandler("clearlocals", clear, run_async=True)
 
 dispatcher.add_handler(EVAL_HANDLER)
 dispatcher.add_handler(EXEC_HANDLER)
 dispatcher.add_handler(CLEAR_HANDLER)
+dispatcher.add_handler(SHELL_HANDLER)
 
+__help__ = """
+*The Following commands are Developer Restricted*
+× /sh
+× /ev
+× /py
+× /Clearlocals
+"""
 __mod_name__ = "Evals"
