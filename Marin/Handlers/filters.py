c7f005f6c77645305a7370e2e8bbae435e0323df
• BHARATHI • <86936565+Bharathi2003@users.noreply.github.com>
2022-02-16 09:13:17 +0530

    Update filters.py

--- Marin/Handlers/filters.py
+++ Marin/Handlers/filters.py
@@ -1,13 +1,13 @@
 from telegram import Message
 from telegram.ext import MessageFilter
 
-from Marin import DEV_USERS, SUPPORT_USERS
+from Marin import DEV_USERS, DRAGONS
 
 
 class CustomFilters(object):
     class _Supporters(MessageFilter):
         def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in SUPPORT_USERS)
+            return bool(message.from_user and message.from_user.id in DRAGONS)
 
     support_filter = _Supporters()
 
