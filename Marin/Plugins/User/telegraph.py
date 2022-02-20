c50cef9f33ecdd0b0eaf46317ead95c3c86e1d56
• BHARATHI • <86936565+Bharathi2003@users.noreply.github.com>
2022-02-20 23:48:04 +0530

    Update telegraph.py

--- Marin/Plugins/User/telegraph.py
+++ Marin/Plugins/User/telegraph.py
@@ -1,16 +1,34 @@
+import os
+import shutil
+from pyrogram import Client, filters
 from telegraph import upload_file
-from pyrogram import filters
-from Marin import bot
+from Marin import TMP_DOWNLOAD_DIRECTORY
+from pyrobot.helper_functions.cust_p_filters import sudo_filter
+from pyrobot.helper_functions.get_file_id import get_file_id
 
 
-@bot.on_message(filters.command('ul'))
-def ul(_, message):
-    reply = message.reply_to_message
-    if reply.media:
-        i = message.reply("**Downloading....**")
-        path = reply.download()
-        fk = upload_file(path)
-        for x in fk:
-            url = "https://telegra.ph" + x
-
-        i.edit(f'Your telegraph [link]({url})', disable_web_page_preview=True)
+@Client.on_message(filters.command("telegraph") & sudo_filter)
+async def telegraph(client, message):
+    replied = message.reply_to_message
+    if not replied:
+        await message.reply_text("Reply to a supported media file")
+        return
+    file_info = get_file_id(replied)
+    if not file_info:
+        await message.reply_text("Not supported!")
+        return
+    _t = os.path.join(TMP_DOWNLOAD_DIRECTORY, str(replied.message_id))
+    if not os.path.isdir(_t):
+        os.makedirs(_t)
+    _t += "/"
+    download_location = await replied.download(_t)
+    try:
+        response = upload_file(download_location)
+    except Exception as document:
+        await message.reply_text(message, text=document)
+    else:
+        await message.reply(
+            f"https://telegra.ph{response[0]}", disable_web_page_preview=True
+        )
+    finally:
+        shutil.rmtree(_t, ignore_errors=True)
