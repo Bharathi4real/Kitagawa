d0c87ffada978f5e8caa655102b7b8b2f5560e79
• BHARATHI • <86936565+Bharathi2003@users.noreply.github.com>
2022-02-20 23:07:50 +0530

    Update notes_sql.py

--- Marin/Database/notes_sql.py
+++ Marin/Database/notes_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from Marin.Handlers.msg_types import Types
 from Marin.Database import BASE, SESSION
-from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
+from sqlalchemy import Boolean, Column, BigInteger, String, UnicodeText, distinct, func
 
 
 class Notes(BASE):
@@ -14,7 +14,7 @@ class Notes(BASE):
     file = Column(UnicodeText)
     is_reply = Column(Boolean, default=False)
     has_buttons = Column(Boolean, default=False)
-    msgtype = Column(Integer, default=Types.BUTTON_TEXT.value)
+    msgtype = Column(BigInteger, default=Types.BUTTON_TEXT.value)
 
     def __init__(self, chat_id, name, value, msgtype, file=None):
         self.chat_id = str(chat_id)  # ensure string
