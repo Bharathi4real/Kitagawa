047a0bfec4366633c737b1fd25df72267d469ebc
• BHARATHI • <86936565+Bharathi2003@users.noreply.github.com>
2022-02-21 08:17:45 +0530

    My bad

--- Marin/Plugins/Sudoers/eval.py
+++ Marin/Plugins/Sudoers/eval.py
@@ -77,7 +77,7 @@ def do(func, bot, update):
 
     os.chdir(os.getcwd())
     with open(
-        os.path.join(os.getcwd(), "Marin/Handlers/temp.txt),
+        os.path.join(os.getcwd(), "Marin/Handlers/temp.txt"),
         "w",
     ) as temp:
         temp.write(body)
