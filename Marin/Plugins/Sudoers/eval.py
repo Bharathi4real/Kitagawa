889c2a8c4f90628b1e41915e76a02984d1a71e89
• BHARATHI • <86936565+Bharathi2003@users.noreply.github.com>
2022-02-21 08:13:41 +0530

    Update eval.py

--- Marin/Plugins/Sudoers/eval.py
+++ Marin/Plugins/Sudoers/eval.py
@@ -77,7 +77,7 @@ def do(func, bot, update):
 
     os.chdir(os.getcwd())
     with open(
-        os.path.join(os.getcwd(), "zeldris/modules/helper_funcs/temp.txt"),
+        os.path.join(os.getcwd(), "Marin/Handlers/temp.txt),
         "w",
     ) as temp:
         temp.write(body)
