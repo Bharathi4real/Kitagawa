6606558fe70b763687ce0949f35c686e6d091056
• BHARATHI • <86936565+Bharathi2003@users.noreply.github.com>
2022-02-27 15:56:53 +0530

    Update sysinfo.py

--- Marin/Plugins/Sudoers/sysinfo.py
+++ Marin/Plugins/Sudoers/sysinfo.py
@@ -100,7 +100,7 @@ def system_status(update: Update, context: CallbackContext):
         + str(telegram.__version__)
         + "</code>\n"
     )
-    context.bot.sendMessage(update.effective_chat.id, status, parse_mode=ParseMode.HTML)
+    context.bot.reply_text(update.effective_chat.id, status, parse_mode=ParseMode.HTML)
 
 
 IP_HANDLER = CommandHandler(
